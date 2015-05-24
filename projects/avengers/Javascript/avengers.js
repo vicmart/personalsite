d9292630517d64188653dd174f7025af2d3b74b3
Vicmart1 <Vicmart1>
2015-05-24 16:42:57 -0400

    added avengers countdown

---
+++ projects/avengers/Javascript/avengers.js
@@ -0,0 +1,51 @@
+$('img').hover(
+	function(){
+    	$(".ironman").css("max-width", "15%");
+    	$(".hulk").css("max-width", "15%");
+    	$(".avengers").css("max-width", "15%");
+    	$(".captain").css("max-width", "15%");
+    	$(".thor").css("max-width", "15%");
+    	$("#" + $(this).parent().attr('id')).css("max-width","40%");
+    	$("#" + $(this).parent().attr('id') + "-clock").removeClass("animated fadeOut").addClass("animated fadeIn");
+	} , 
+	function(){
+		$(".ironman").css("max-width", "20%");
+		$(".hulk").css("max-width", "20%");
+    	$(".avengers").css("max-width", "20%");
+    	$(".captain").css("max-width", "20%");
+    	$(".thor").css("max-width", "20%");
+    	$("#" + $(this).parent().attr('id') + "-clock").removeClass("animated fadeIn").addClass("animated fadeOut");
+	}
+);
+
+$( document ).ready(function() {
+	$("#ironman-clock").countdown("2016/05/06", function(event) {
+		$(this).text(
+			event.strftime('%w weeks %-d days %-H:%M:%S')
+		);
+	});
+
+	$("#hulk-clock").countdown("2018/05/04", function(event) {
+		$(this).text(
+			event.strftime('%w weeks %-d days %-H:%M:%S')
+		);
+	});
+
+	$("#avengers-clock").countdown("2018/05/04", function(event) {
+		$(this).text(
+			event.strftime('%-w weeks %-d days %-H:%M:%S')
+		);
+	});
+	
+	$("#captain-clock").countdown("2016/05/06", function(event) {
+		$(this).text(
+			event.strftime('%w weeks %-d days %-H:%M:%S')
+		);
+	});
+	
+	$("#thor-clock").countdown("2017/11/03", function(event) {
+		$(this).text(
+			event.strftime('%w weeks %-d days %-H:%M:%S')
+		);
+	});
+});
