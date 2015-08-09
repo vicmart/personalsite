e8e0acc62cafae555454ea3dcf2031e2eb9880b0
Vicmart1 <Vicmart1>
2015-08-09 09:46:36 -0400

    added grow animations

--- mobile/Javascript/square.js
+++ mobile/Javascript/square.js
@@ -1,23 +1,40 @@
 var window_width = window.innerWidth;
 var window_height = window.innerHeight;
+var target;
+var scroll = 0;
 
-$(".bsox").hover(
+$(".container").hover(
 function() {
 	$(this).data("width", $(this).css("width"));
 	$(this).data("height", $(this).css("height"));
-	$(this).css("width", (window_width * 2.0) + "px");
-	$(this).css("height", (window_height * 3.0) + "px");
-	$(this).css("left", (window_width * -0.54) + "px");
-	$(this).css("top", (window_height * -1.0) + "px");
+	$(this).data("left", $(this).css("left"));
+	$(this).data("top", $(this).css("top"));
+	$(this).data("margin", $(this).css("margin"));
+	$(this).data("padding", $(this).css("padding"));
+	$(this).css("width", "calc(100%)");
+	$(this).css("height", "100vh");
+	$(this).css("left", "0px");
+	$(this).css("top", $(window).scrollTop() + "px");
+	$(this).css("margin", "0px");
+	$(this).css("padding", "0px");
 	$(this).css("z-index", 1);
-	console.log($(this).parent().css("left"));
+	target = $(this);
 },
 function() {
 	$(this).css("width", $(this).data("width"));
 	$(this).css("height", $(this).data("height"));
-	$(this).css("left", "0px");
-	$(this).css("top", "0px");
-	$(this).css("z-index", 0);
+	$(this).css("left", $(this).data("left"));
+	$(this).css("top", $(this).data("top"));
+	$(this).css("margin", $(this).data("margin"));
+	$(this).css("padding", $(this).data("padding"));
+
+	$(target).css("z-index", 0);
+	target = null;
+});
+
+$(window).scroll(function(){
+    scroll = $(this).scrollTop();
+	$(target).css("top", scroll + "px");
 });
 
 $( document ).ready(function() {
