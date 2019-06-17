8a8d41d491692bec5bb31cbcdfe552faf14a35e2
Vicmart1 <vdadfar1@gmail.com>
2019-06-17 00:08:06 -0400

    work on v10

--- v10/index.js
+++ v10/index.js
@@ -1,22 +1,31 @@
-const blockSize = 30;
-let blockMargin = 5;
+let blockSize = 15;
 
 $(document).ready(function() {
-  let amount = (window.innerWidth + blockMargin) / (blockSize + blockMargin);
-  let leftover =  (window.innerWidth + blockMargin) % (blockSize + blockMargin);
-  blockMargin += leftover / (amount * 1.0);
+  let amount = window.innerWidth / blockSize;
+  let leftover =  window.innerWidth % blockSize;
+  blockSize += leftover / (amount * 1.0);
 
-  for (let x = 0; x < (window.innerWidth + blockMargin) / (blockSize + blockMargin); x++) {
-    for (let y = 0; y < (window.innerHeight + blockMargin) / (blockSize + blockMargin); y++) { 
+  for (let x = 0; x < window.innerWidth / blockSize; x++) {
+    for (let y = 0; y < window.innerHeight / blockSize; y++) { 
       let block = document.createElement('div');
       block.classList.add('Block');
+      block.id = `js-b-${x}-${y}`;
       block.style.width = blockSize + 'px';
       block.style.height = blockSize + 'px';
-      block.style.marginTop = blockMargin;
-      block.style.marginLeft = blockMargin;
-      block.style.left = (x * (blockMargin + blockSize) - blockMargin) + "px";
-      block.style.top = (y * (blockMargin + blockSize) - blockMargin) + "px";
+      block.style.left = (x * blockSize) + "px";
+      block.style.top = (y * blockSize) + "px";
       document.body.appendChild(block);
     }
   }
+
+  document.addEventListener('mouseover', (e) => {
+    let mouseX = e.clientX;
+    let mouseY = e.clientY;
+
+    let blockX = Math.floor(mouseX / blockSize);
+    let blockY = Math.floor(mouseY / blockSize);
+    
+    let target = document.querySelector(`#js-b-${blockX}-${blockY}`);
+    console.log(`#js-b-${blockX}-${blockY}`);
+  });
 });
