9828981af385f2cf404318d7d1c9a5bac1b4a9ca
Vicmart1 <vdadfar1@gmail.com>
2020-02-23 02:06:54 -0500

    a bit of neo skeumorphism added

--- v10/src/index.js
+++ v10/src/index.js
@@ -2,14 +2,10 @@ window.onload = () => {
   let section1 = document.querySelector('.js-section-1');
   let section2 = document.querySelector('.js-section-2');
 
-  let background = document.querySelector('.js-background');
-
   document.addEventListener('mousemove', (e) => {
-    let newWidth1 = Math.max(0, e.clientX - 16);
-    let newWidth2 = Math.max(0, window.innerWidth - newWidth1 - 32);
+    let newWidth1 = Math.max(0, e.clientX);
+    let newWidth2 = Math.max(0, window.innerWidth - newWidth1);
     section1.style.width = `${newWidth1}px`;
     section2.style.width = `${newWidth2}px`;
-    
-    background.style.opacity = Math.min(1, Math.max(0, newWidth2/(window.innerWidth - 30)));
   });
 }
