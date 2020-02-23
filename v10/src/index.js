18d25ef2310ee5fd024e81a6af78d7a21351b38f
Vicmart1 <vdadfar1@gmail.com>
2020-02-23 01:27:01 -0500

    work on v10

--- v10/src/index.js
+++ v10/src/index.js
@@ -2,10 +2,14 @@ window.onload = () => {
   let section1 = document.querySelector('.js-section-1');
   let section2 = document.querySelector('.js-section-2');
 
+  let background = document.querySelector('.js-background');
+
   document.addEventListener('mousemove', (e) => {
-    let newWidth1 = (window.innerWidth/2) + (1.15 * (e.clientX - 15 - (window.innerWidth/2)));
-    let newWidth2 = window.innerWidth - newWidth1 - 30;
+    let newWidth1 = Math.max(0, e.clientX - 16);
+    let newWidth2 = Math.max(0, window.innerWidth - newWidth1 - 32);
     section1.style.width = `${newWidth1}px`;
-    section2.style.width = `${newWidth2}`;
+    section2.style.width = `${newWidth2}px`;
+    
+    background.style.opacity = Math.min(1, Math.max(0, newWidth2/(window.innerWidth - 30)));
   });
 }
